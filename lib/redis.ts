a2ccdd1e3fc77a755c60ab7e7b0931e103491809
Nikola Mirilo <nikolamirilo@gmail.com>
2025-01-07 16:07:49 +0100

    removed redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis("rediss://default:AbGnAAIjcDE0MGZmMGVlOWFiZjI0OWYyYTA3MjJkZjg3N2YxMjlkOXAxMA@bursting-moray-45479.upstash.io:6379");
+export const redisClient = new Redis(process.env.REDIS_CONNECTION_STRING!);
