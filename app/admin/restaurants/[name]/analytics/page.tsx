0eefd5ac6f2f1661cccfb28c2b11949bd26971af
Nikola <59510666+nikolamirilo@users.noreply.github.com>
2025-06-26 07:49:51 +0200

    Update page.tsx

--- app/admin/restaurants/[name]/analytics/page.tsx
+++ app/admin/restaurants/[name]/analytics/page.tsx
@@ -1,14 +1,18 @@
 import Analytics from '@/components/dashboard/Analytics';
 import React from 'react'
+import Navbar from "@/components/navigation/Navbar"
 
 type tParams = Promise<{ name: string }>;
 
 export default async function page({ params }: { params: tParams}) {
   const { name } = await params
   return (
-      <div className='flex w-full gap-10 justify-center items-center flex-col relative'>
-      <h1 className='text-3xl text-secondary'>Restaurant Dashboard {name}</h1>
-      <Analytics />
-      </div>
+      <>
+        <Navbar/>
+        <div className='py-20 flex w-full gap-10 justify-center items-center flex-col relative'>
+        <h1 className='text-3xl text-secondary'>Restaurant Dashboard {name}</h1>
+        <Analytics />
+        </div>
+      </>
   )
 }
