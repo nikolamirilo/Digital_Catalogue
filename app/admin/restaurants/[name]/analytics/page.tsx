5299525abc18d18afb84b3c67996be6d4e584377
Nikola <59510666+nikolamirilo@users.noreply.github.com>
2025-06-26 07:55:01 +0200

    Update page.tsx

--- app/admin/restaurants/[name]/analytics/page.tsx
+++ app/admin/restaurants/[name]/analytics/page.tsx
@@ -6,11 +6,15 @@ type tParams = Promise<{ name: string }>;
 
 export default async function page({ params }: { params: tParams}) {
   const { name } = await params
+  const transformedName = name
+  .split("-")
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ");
   return (
       <>
         <Navbar/>
-        <div className='py-20 flex w-full gap-10 justify-center items-center flex-col relative'>
-        <h1 className='text-3xl text-secondary'>Restaurant Dashboard {name}</h1>
+        <div className='py-24 flex w-full gap-10 justify-center items-center flex-col relative'>
+        <h1 className='text-4xl text-white font-bold'>Analytics for restaurant {transformedName}</h1>
         <Analytics />
         </div>
       </>
